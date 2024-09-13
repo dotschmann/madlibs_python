2d7666eebb29c2cb9059db1018490617778061cc
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-13 23:50:53 +0200

    function created for random number

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import random
 
-name = input("Enter your name?: ")
+# name = input("Enter your name?: ")
 
-print(f"Hello {name}! Here is your story:")
+# print(f"Hello {name}! Here is your story:")
 
-print("")
+# print("")
 
-l = int(random.randint(1, 3))
-m = l - 1
+def randNum():
+    l = (random.randint(1, 3))
+    return  l - 1
 
-# print(l)
+
 
 a = ["mutige", "junge", "kluge"]
 b = ["sonnigen", "windigen", "regnerischen"]
@@ -21,11 +22,24 @@ g = ["nervös", "neugierig", "aufgeregt"]
 h = ["ruhig", "mysteriös", "lebendig"] 
 i = ["dunkle", "hohe", "elegante"]
 j = ["mutig", "entspannt", "beschwingt"]
-
-print(f"Der {a[m]} Max ging an einem {b[m]} Morgen in den Wald.") 
-print(f"Er trug seinen {c[m]} Rucksack und summte ein {d[m]} Lied.")
-print(f"Plötzlich hörte er ein {e[m]} Geräusch.")
-print(f"Max blieb stehen, drehte sich um und sah ein {f[m]} Tier, das zwischen den Bäumen verschwand.")
-print(f"Obwohl er etwas {g[m]} war, beschloss er, weiterzugehen.")
-print(f"Der Wald war {h[m]}, und die Bäume warfen {i[m]} Schatten auf den Weg.")
-print(f"Max fühlte sich {j[m]} und setzte seine Wanderung fort.")
+m = randNum()
+print(m)
+print(f"Der '{a[m]}' Max ging an einem '{b[m]}' Morgen in den Wald.") 
+m = randNum()
+print(m)
+print(f"Er trug seinen '{c[m]}' Rucksack und summte ein '{d[m]}' Lied.")
+m = randNum()
+print(m)
+print(f"Plötzlich hörte er ein '{e[m]}' Geräusch.")
+m = randNum()
+print(m)
+print(f"Max blieb stehen, drehte sich um und sah ein '{f[m]}' Tier, das zwischen den Bäumen verschwand.")
+m = randNum()
+print(m)
+print(f"Obwohl er etwas '{g[m]}' war, beschloss er, weiterzugehen.")
+m = randNum()
+print(m)
+print(f"Der Wald war '{h[m]}', und die Bäume warfen '{i[m]}' Schatten auf den Weg.")
+m = randNum()
+print(m)
+print(f"Max fühlte sich '{j[m]}' und setzte seine Wanderung fort.")
