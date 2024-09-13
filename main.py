b08ccb5609a82df80d4b99d68a8d5dade66417d3
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-13 23:31:28 +0200

    random adjective generated for the story

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import random
+
+name = input("Enter your name?: ")
+
+print(f"Hello {name}! Here is your story:")
+
+print("")
+
+l = int(random.randint(1, 3))
+m = l - 1
+
+# print(l)
+
 a = ["mutige", "junge", "kluge"]
 b = ["sonnigen", "windigen", "regnerischen"]
 c = ["großen", "schweren", "bunten"]
@@ -5,14 +18,14 @@ d = ["fröhliches", "leises", "schönes"]
 e = ["lautes", "komisches", "gruseliges"]
 f = ["kleines", "schnelles", "schwarzes"]
 g = ["nervös", "neugierig", "aufgeregt"]
-h = ["ruhig", "mysteriös", "lebendig"]
+h = ["ruhig", "mysteriös", "lebendig"] 
 i = ["dunkle", "hohe", "elegante"]
 j = ["mutig", "entspannt", "beschwingt"]
 
-print(f"Der {a[1]} Max ging an einem {b[1]} Morgen in den Wald.")
-print(f"Er trug seinen {c[1]} Rucksack und summte ein {d[1]} Lied.")
-print(f"Plötzlich hörte er ein {e[1]} Geräusch.")
-print(f"Max blieb stehen, drehte sich um und sah ein {f[1]} Tier, das zwischen den Bäumen verschwand.")
-print(f"Obwohl er etwas {g[1]} war, beschloss er, weiterzugehen.")
-print(f"Der Wald war {h[1]}, und die Bäume warfen {i[1]} Schatten auf den Weg.")
-print(f"Max fühlte sich {j[1]} und setzte seine Wanderung fort.")
+print(f"Der {a[m]} Max ging an einem {b[m]} Morgen in den Wald.") 
+print(f"Er trug seinen {c[m]} Rucksack und summte ein {d[m]} Lied.")
+print(f"Plötzlich hörte er ein {e[m]} Geräusch.")
+print(f"Max blieb stehen, drehte sich um und sah ein {f[m]} Tier, das zwischen den Bäumen verschwand.")
+print(f"Obwohl er etwas {g[m]} war, beschloss er, weiterzugehen.")
+print(f"Der Wald war {h[m]}, und die Bäume warfen {i[m]} Schatten auf den Weg.")
+print(f"Max fühlte sich {j[m]} und setzte seine Wanderung fort.")
