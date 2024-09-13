2cbe6aa813b64548f30d97117cfd564cf6a8bbbf
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-13 23:00:23 +0200

    create lists of adjectives to be use in story

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-# Ask user input
+a = ["mutige", "junge", "kluge"]
+b = ["sonnigen", "windigen", "regnerischen"]
+c = ["großen", "schweren", "bunten"]
+d = ["fröhliches", "leises", "schönes"]
+e = ["lautes", "komisches", "gruseliges"]
+f = ["kleines", "schnelles", "schwarzes"]
+g = ["nervös", "neugierig", "aufgeregt"]
+h = ["ruhig", "mysteriös", "lebendig"]
+i = ["dunkle", "hohe", "elegante"]
+j = ["mutig", "entspannt", "beschwingt"]
 
-name = input("What is your name?: ")
-
-print(f"Hello {name}! Welcome to Madlib Game!")
+print(f"Der {a[1]} Max ging an einem {b[1]} Morgen in den Wald.")
+print(f"Er trug seinen {c[1]} Rucksack und summte ein {d[1]} Lied.")
+print(f"Plötzlich hörte er ein {e[1]} Geräusch.")
+print(f"Max blieb stehen, drehte sich um und sah ein {f[1]} Tier, das zwischen den Bäumen verschwand.")
+print(f"Obwohl er etwas {g[1]} war, beschloss er, weiterzugehen.")
+print(f"Der Wald war {h[1]}, und die Bäume warfen {i[1]} Schatten auf den Weg.")
+print(f"Max fühlte sich {j[1]} und setzte seine Wanderung fort.")
