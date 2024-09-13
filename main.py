ff6a6b5086f3026d25456ffdcfe07ce1dc7de823
dotschmann <73004827+dotschmann@users.noreply.github.com>
2024-09-14 00:07:54 +0200

    increase number of adjective options. player name is asked and welcomed

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import random
 
-# name = input("Enter your name?: ")
+name = input("Enter your name?: ")
 
-# print(f"Hello {name}! Here is your story:")
+print(f"Hello {name}! Here is your story:")
 
-# print("")
-
+print("")
+ 
 def randNum():
-    l = (random.randint(1, 3))
+    l = (random.randint(1, 8))
     return  l - 1
 
 
 
-a = ["mutige", "junge", "kluge"]
-b = ["sonnigen", "windigen", "regnerischen"]
-c = ["großen", "schweren", "bunten"]
-d = ["fröhliches", "leises", "schönes"]
-e = ["lautes", "komisches", "gruseliges"]
-f = ["kleines", "schnelles", "schwarzes"]
-g = ["nervös", "neugierig", "aufgeregt"]
-h = ["ruhig", "mysteriös", "lebendig"] 
-i = ["dunkle", "hohe", "elegante"]
-j = ["mutig", "entspannt", "beschwingt"]
+a = ["mutige", "junge", "kluge", "tapfere", "witzige", "starke", "geschickte", "geduldige", "ehrliche", "freundliche"]
+b = ["sonnigen", "windigen", "regnerischen", "nebligen", "stürmischen", "klaren", "kühlen", "heiteren", "frostigen", "trockenen"]
+c = ["großen", "schweren", "bunten", "kleinen", "leichten", "alten", "neuen", "engen", "weichen", "robusten"]
+d = ["fröhliches", "leises", "schönes", "melodisches", "schnelles", "lustiges", "trauriges", "sanftes", "kraftvolles", "harmonisches"]
+e = ["lautes", "komisches", "gruseliges", "mysteriöses", "scharfes", "helles", "dumpfes", "schrilles", "seltsames", "monotones"]
+f = ["kleines", "schnelles", "schwarzes", "weißes", "braunes", "leises", "wildes", "freundliches", "gefährliches", "zartes"]
+g = ["nervös", "neugierig", "aufgeregt", "ängstlich", "überrascht", "entschlossen", "verwirrt", "gelassen", "angespannt", "aufmerksam"]
+h = ["ruhig", "mysteriös", "lebendig", "friedlich", "einsam", "belebend", "bedrohlich", "still", "verträumt", "abenteuerlich"]
+i = ["dunkle", "hohe", "elegante", "tiefe", "breite", "schmale", "klare", "verschwommene", "glänzende", "sanfte"]
+j = ["mutig", "entspannt", "beschwingt", "glücklich", "furchtlos", "optimistisch", "kraftvoll", "zufrieden", "ausgeglichen", "frisch"]
+
 
 m = randNum()
 print(f"Der '{a[m]}' Max ging an einem '{b[m]}' Morgen in den Wald.") 
